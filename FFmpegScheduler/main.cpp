02943c8e3eac65b11a1cbafc1a3d045054652e92
iriszero <ih@iriszero.cc>
2024-03-16 11:51:07 +0800

    update CuRanges

--- FFmpegScheduler/main.cpp
+++ FFmpegScheduler/main.cpp
@@ -27,40 +27,6 @@
 #include <WinUser.h>
 #endif
 
-#ifdef __cpp_lib_ranges
-#define CU_RANGES_HAS_STD_RANGES
-#include <ranges>
-#elif defined(CU_RANGES_USE_RANGE_V3)
-#define CU_RANGES_HAS_RANGE_V3
-#endif
-
-namespace CuRanges
-{
-	template <typename T>
-	void StableSort(T& cont)
-	{
-#ifdef CU_RANGES_HAS_STD_RANGES
-		std::ranges::stable_sort(cont);
-#elif defined(CU_RANGES_HAS_RANGE_V3)
-
-#else
-		std::stable_sort(cont.begin(), cont.end());
-#endif
-	}
-
-	template <typename T, typename F>
-	void Generate(T& cont, F&& func)
-	{
-#ifdef CU_RANGES_HAS_STD_RANGES
-		std::ranges::generate(cont, std::forward<F>(func));
-#elif defined(CU_RANGES_HAS_RANGE_V3)
-
-#else
-		std::generate(cont.begin(), cont.end(), std::forward<F>(func));
-#endif
-	}
-}
-
 [[nodiscard]] std::string PresetDesc()
 {
 	std::ostringstream ss;
